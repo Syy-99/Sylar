3bb351023306fd85480e7606be81295d90d83b73
syy-server <lab_server>
2023-05-12 21:40:14 +0800

    config05

--- README.md
+++ README.md
@@ -17,3 +17,25 @@ C++高性能服务器框架
 ## 配置系统
 
 Config -> Yaml
+
+- 支持复杂类型的Yaml， 配置的值可以允许vector,map,set，自定义class
+
+    - 简单类型： 用boost:lexical_cast内置转换方式
+
+    - 复杂类型： 提供自定义的解析方法 -> 序列化与反序列化
+
+        - 利用模版偏特例化
+
+        > 序列化是指把对象转换为字节序列
+        > 
+        > 反序列化是指把字节序列恢复为Java对象的过程：
+        >
+        > https://www.finclip.com/news/f/21276.html |  https://juejin.cn/post/7064942360106369054
+
+- 配置系统的原则——约定优于配置： https://blog.csdn.net/ThinkWon/article/details/101703815，是一种软件设计范式。com/yanggb/p/10589073.html
+
+    - 本质来说，系统、类库或框架应该假定合理的默认值，而非要求提供不必要的配置
+
+    - 通过约定来减少配置
+
+    - 只会配置约定的参数
--- sylar/config.h
+++ sylar/config.h
@@ -44,11 +44,76 @@ namespace sylar {
         std::string m_description;  // 配置参数的描述 
     };
 
+    /**
+    * @brief 类型转换模板类(F 源类型, T 目标类型)
+    */
+    template<class F, class T>
+    class LexicalCast{
+    public:
+        /**
+        * @brief 类型转换
+        * @param[in] v 源类型值
+        * @return 返回v转换后的目标类型
+        * @exception 当类型不可转换时抛出异常
+        */
+        T operator()(const F& v) {
+            return boost::lexical_cast<T>(v);
+        }
+    };  
+
+    /// 类型转换模板类偏特化(YAML String 转换成 std::vector<T>)
+    template<class T>
+    class LexicalCast<std::string, std::vector<T> > {
+    public:
+        std::vector<T> operator()(const std::string& v) {
+            /*
+            key:
+               - 1
+               - 2
+            or
+            key: [10,20]
+
+            只有这种类型的yaml可以转换为vector<T>类型，否则会抛出异常，因此调用该函数的函数需要catch该异常
+            */
+            YAML::Node node = YAML::Load(v);
+
+            typename std::vector<T> vec;
+            std::stringstream ss;
+            for(size_t i = 0; i < node.size(); ++i) {
+                ss.str("");
+                ss << node[i];
+                vec.push_back(LexicalCast<std::string, T>()(ss.str()));
+            } 
+            return vec;
+        }
+    };
+
+    /**
+    * @brief 类型转换模板类片特化(std::vector<T> 转换成 YAML String)
+    */
+    template<class T>
+    class LexicalCast<std::vector<T>, std::string> {
+    public:
+        std::string operator()(const std::vector<T>& v) {
+            YAML::Node node(YAML::NodeType::Sequence);
+            for(auto& i : v) {
+                node.push_back(YAML::Load(LexicalCast<T, std::string>()(i)));
+            }
+            std::stringstream ss;
+            ss << node;
+            return ss.str();
+        }
+    };
     
     /**
-     * @brief 配置参数模板子类,保存对应类型的参数值 -> 具体配置项
-     */
-    template<class T>
+    * @brief 配置参数模板子类,保存对应类型的参数值
+    * @details T 参数的具体类型
+    *          FromStr 从std::string转换成T类型的仿函数 -> 序列化 
+    *          ToStr 从T转换成std::string的仿函数   -> 反序列化
+    *          std::string 为YAML格式的字符串
+    */
+    template<class T, class FromStr = LexicalCast<std::string, T>,
+                      class ToStr = LexicalCast<T, std::string> >
     class ConfigVar : public ConfigVarBase {
     public:
         typedef std::shared_ptr<ConfigVar> ptr;
@@ -64,7 +129,7 @@ namespace sylar {
         // 将参数值转换成YAML String
         std::string toString() override {
             try {
-                return boost::lexical_cast<std::string>(m_val);
+                 return ToStr()(m_val);
             } catch (std::exception& e) {
                 SYLAR_LOG_ERROR(SYLAR_LOG_ROOT()) << "ConfigVar::toString exception"
                                                    << e.what() 
@@ -75,7 +140,7 @@ namespace sylar {
 
         bool fromString(const std::string& val) override {
             try {
-                m_val = boost::lexical_cast<T>(val);
+                setValue(FromStr()(val));
                 return true;
             } catch (std::exception& e) {
                   SYLAR_LOG_ERROR(SYLAR_LOG_ROOT()) << "ConfigVar::fromString exception"
