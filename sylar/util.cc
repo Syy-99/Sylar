10625b4be97295a27d5f4b9d37aa2fbca16c1af6
syy-server <lab_server>
2023-05-18 22:35:21 +0800

    协程模式01-断言宏

--- sylar/util.cc
+++ sylar/util.cc
@@ -1,13 +1,48 @@
 #include "util.h"
+#include <execinfo.h>
 
+#include "log.h"
 
 namespace sylar {
+
+sylar::Logger::ptr g_logger = SYLAR_LOG_NAME("system");     // 获得日志器
     
-    pid_t GetThreadId() {
-        return syscall(SYS_gettid);     // 利用系统调用获得线程ID
+pid_t GetThreadId() {
+    return syscall(SYS_gettid);     // 利用系统调用获得线程ID
+}
+
+uint32_t GetFiberId() {
+    return 0;
+}
+
+void Backtrace(std::vector<std::string>& bt, int size, int skip)  {
+    void** array = (void**)malloc((sizeof(void*) * size));      // 用堆空间而不用栈空间(协程的栈空间小，可以快速切换)
+    size_t s = ::backtrace(array, size);        // 返回真实层数
+
+    char **strings = ::backtrace_symbols(array, s);
+    if(strings == NULL) {
+        SYLAR_LOG_ERROR(g_logger) << "backtrace_synbols error";
+        return;
     }
 
-    uint32_t GetFiberId() {
-        return 0;
+    for(size_t i = skip; i < s; ++i) {
+        bt.push_back(strings[i]);     // 将调用栈的名字保存到vecotr中
     }
+
+    free(strings);
+    free(array);
+}
+
+std::string BacktraceToString(int size, int skip, const std::string& prefix) {
+    std::vector<std::string> bt;
+    Backtrace(bt, size, skip);
+    std::stringstream ss;
+
+    for(size_t i = 0; i < bt.size(); ++i) {
+        ss << prefix << bt[i] << std::endl;
+    }
+
+    return ss.str();
+}
+
 }
