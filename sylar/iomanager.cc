21e7a53e15e8d5c71c00a269f1e9d13d4622b1da
syy-server <lab_server>
2023-05-28 21:44:42 +0800

    IO协程调度模块02

--- sylar/iomanager.cc
+++ sylar/iomanager.cc
@@ -5,10 +5,47 @@
 #include <sys/epoll.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
+#include <string.h>
 
 namespace sylar {
 
 static sylar::Logger::ptr g_logger = SYLAR_LOG_NAME("system");
+
+IOManager::FdContext::EventContext& IOManager::FdContext::getContext(IOManager::Event event) {
+    switch(event) {
+        case IOManager::READ:
+            return read;
+        case IOManager::WRITE:
+            return write;
+        default:        // 不可能是读和写吗?
+            SYLAR_ASSERT2(false, "getContext");
+    }
+    throw std::invalid_argument("getContext invalid event");
+}
+
+/// 重置该fd上的某个事件上下文
+void IOManager::FdContext::resetContext(EventContext& ctx) {
+    ctx.scheduler = nullptr;
+    ctx.fiber.reset();
+    ctx.cb = nullptr;
+}
+
+void IOManager::FdContext::triggerEvent(IOManager::Event event) {
+    SYLAR_ASSERT(events & event);   // 确保有这个监听事件
+
+    events = (Event)(events & ~event);  // 删除这个事件
+
+    EventContext& ctx = getContext(event);
+    if(ctx.cb) {
+        ctx.scheduler->schedule(&ctx.cb);   // 将其加入调度器中，让某个线程中的协程执行一下这个任务
+    } else {
+        ctx.scheduler->schedule(&ctx.fiber);
+    }
+
+    ctx.scheduler = nullptr;
+    return;
+}
 
 IOManager::IOManager(size_t threads = 1, bool use_caller = true, const std::string& name = "") 
     : Scheduler(threads, use_caller, name){
@@ -77,12 +114,172 @@ int IOManageradd::addEvent(int fd, Event event, std::function<void()> cb = nullp
     }
 
     FdContext::MutexType::Lock lock2(fb_ctx->mutex);
-    if(fb_ctx->events & events) {
-       // SYLAR_LOG_ERROR(g_logger) << "state "
+    if(fb_ctx->events & events) {   // 事件相同
+        // 这个事件可能被其他线程加入过
+       SYLAR_LOG_ERROR(g_logger) << "addEvent error" << fd
+                                 << "event= " << event;
+                                 << " fd_ctx.event=" << fb_ctx->events;
+        SYLAR_ASSERT(!(fb_ctx->events & event));
     }
 
+    int op = fd_ctx->events ? EPOLL_CTL_MOD : EPOLL_CTL_ADD;    // 判断是修改还是增加
+    epoll_event epevent;
+    epevent.events = EPOLLET | fd_ctx->events | event;
+    epevent.data.ptr = fd_ctx;      // ???保存该事件的fdcContexts
+
+    int rt = epoll_ctl(m_epfd, op, fd, &epevent);
+    if (rt) {
+        SYLAR_LOG_ERROR << "epoll_ctl(" << m_epfd <<","
+            << op << ", " << fd << ", " << epevent.events<<")"
+            << rt << " (" << errno <<") (" <<strerror(errno) << ")";
+
+        return -1;
+    }
+
+    ++m_pendingEventCount;      //? 如果这个事件本身就有，那这个数字不就多加了???
+
+    fd_ctx->events = (Event)(fd_ctx->events | event);   // 理解这里的或： 可读可写事件
+
+    FdContext::EventContext& event_ctx = fd_ctx->getContext(event);
+    SYLAR_ASSERT(!event_ctx.scheduler   // 确保为空
+                && !event_ctx.fiber
+                && !event_ctx.cb);
+    // ?? 这里不是可以修改的吗? 为什们不允许之前有值呢?
+
+    event_ctx.scheduler = Scheduler::GetThis();     // 设置为当前Scheduler
+    if(cb) {
+        event_ctx.cb.swap(cb);
+    } else {
+        event_ctx.fiber = Fiber::GetThis(); // 这里的逻辑是什么？
+        SYLAR_ASSERT2(event_ctx.fiber->getState() == Fiber::EXEC);
+    }
+
+    return 0;
 }
-bool IOManager::delEvent(int fd, Event event);
-bool IOManager::cancelEvent(int fd, Event event);
+
+bool IOManager::delEvent(int fd, Event event) {
+    RWMutexType::ReadLock lock(m_mutex);
+    if((int)m_fdContexts.size() <= fd) {
+        return false;
+    }
+    FdContext* fd_ctx = m_fdContexts[fd];   // 获得这个fd的上下文
+    lock.unlock();
+
+    FdContext::MutexType::Lock lock2(fd_ctx->mutex);
+    if(fd_ctx->events & event) {    // fd中没有这个事件
+        return false;   // 则不用删除
+    }
+
+    Event new_events = (Event)(fd_ctx->events & ~event);
+    int op = new_events ? EPOLL_CTL_MOD : EPOLL_CTL_DEL;        // 判断是修改这个fd,还是直接删除fd
+
+    // 重新设置这个fd上的事件
+    epoll_event epevent;
+    epevent.events = EPOLLET | new_events;
+    epevent.data.ptr = fd_ctx;
+
+    // 重新设置这个fd的监听情况
+    int rt = epoll_ctl(m_epfd, op, fd, &epevent);
+    if (rt) {
+        SYLAR_LOG_ERROR << "epoll_ctl(" << m_epfd <<","
+            << op << ", " << fd << ", " << epevent.events<<")"
+            << rt << " (" << errno <<") (" <<strerror(errno) << ")";
+
+        return false;
+    }
+
+    --m_pendingEventCount;
+
+    fd_ctx->events = new_events;
+    FdContext::EventContext& event_ctx = fd_ctx->getContext(event);
+    fd_ctx->resetContext(event_ctx);
+    return true;
+}
+bool IOManager::cancelEvent(int fd, Event event) {
+    RWMutexType::ReadLock lock(m_mutex);
+    if((int)m_fdContexts.size() <= fd) {
+        return false;
+    }
+    FdContext* fd_ctx = m_fdContexts[fd];   // 获得这个fd的上下文
+    lock.unlock();
+
+    FdContext::MutexType::Lock lock2(fd_ctx->mutex);
+    if(fd_ctx->events & event) {    // fd中没有这个事件
+        return false;   // 则不用删除
+    }
+
+    Event new_events = (Event)(fd_ctx->events & ~event);
+    int op = new_events ? EPOLL_CTL_MOD : EPOLL_CTL_DEL;        // 判断是修改这个fd,还是直接删除fd
+
+    // 重新设置这个fd上的事件
+    epoll_event epevent;
+    epevent.events = EPOLLET | new_events;
+    epevent.data.ptr = fd_ctx;
+
+    // 重新设置这个fd的监听情况
+    int rt = epoll_ctl(m_epfd, op, fd, &epevent);
+    if (rt) {
+        SYLAR_LOG_ERROR << "epoll_ctl(" << m_epfd <<","
+            << op << ", " << fd << ", " << epevent.events<<")"
+            << rt << " (" << errno <<") (" <<strerror(errno) << ")";
+
+        return false;
+    }
+
+    fd_ctx->triggerEvent(event)
+    // FdContext::EventContext& event_ctx = fd_ctx->getContext(event);
+    // event_ctx->triggerEvent(event);     // 如果该事件被注册过回调，那就触发一次回调事件
+    --m_pendingEventCount;
+    return true;
+}
+
+bool IOManager::cancelAll(int fd){
+    
+    RWMutexType::ReadLock lock(m_mutex);
+    if((int)m_fdContexts.size() <= fd) {
+        return false;
+    }
+    FdContext* fd_ctx = m_fdContexts[fd];
+    lock.unlock();
+
+    FdContext::MutexType::Lock lock2(fd_ctx->mutex);
+    if(!fd_ctx->events) {       // 如果这个fd本身就没有事件
+        return false;
+    }
+
+    int op = EPOLL_CTL_DEL;     // 删除
+    epoll_event epevent;
+    epevent.events = 0;
+    epevent.data.ptr = fd_ctx;
+
+    int rt = epoll_ctl(m_epfd, op, fd, &epevent);
+    if(rt) {
+        SYLAR_LOG_ERROR(g_logger) << "epoll_ctl(" << m_epfd << ", "
+            << (EpollCtlOp)op << ", " << fd << ", " << (EPOLL_EVENTS)epevent.events << "):"
+            << rt << " (" << errno << ") (" << strerror(errno) << ")";
+        return false;
+    }
+
+    if(fd_ctx->events & READ) {
+        fd_ctx->triggerEvent(READ)
+        // FdContext::EventContext& event_ctx = fd_ctx->getContext(READ);
+        // event_ctx->triggerEvent(event);     // 如果该事件被注册过回调，那就触发一次回调事件
+        --m_pendingEventCount;
+    }
+    if(fd_ctx->events & WRITE) {
+        fd_ctx->triggerEvent(WRITE)
+        // FdContext::EventContext& event_ctx = fd_ctx->getContext(WRITE);
+        // event_ctx->triggerEvent(event);     // 如果该事件被注册过回调，那就触发一次回调事件
+        --m_pendingEventCount;
+    }
+
+    SYLAR_ASSERT(fd_ctx->events == 0);
+    return true;
+}
+
+IOManager* GetThis() {
+    // 返回当前线程使用的IOManager
+    return dynamic_cast<IOManager*>(Scheduler::GetThis());
+}
 
 }
--- sylar/iomanager.h
+++ sylar/iomanager.h
@@ -24,6 +24,7 @@ private:
     // Socket事件对应的类
     struct FdContext {
         typedef Mutex MutexType;
+        /// 事件上下文
         struct EventContext {
              /// 事件执行的调度器: 事件由哪个调度器控制
             Scheduler* scheduler = nullptr;
@@ -33,7 +34,18 @@ private:
             /// 事件的回调函数
             std::function<void()> cb;            
         }
+
+        /// 获取事件上下文类
+        /// 根据传入的event，来判断最终的类型 读 or 写 or 读写
+        // ?? 这里有没有可能一个fd上同时有读 写事件，那不就要返回两个??
+        EventContext& getContext(Event event);  
+
+        /// 重置该fd上的某个事件上下文
+        void resetContext(EventContext& ctx);
+
+        void triggerEvent(Event ctx);
         
+        /// 每个fd上只会有两种事件，但是可以同时存在
         /// 读事件上下文
         EventContext read;
         /// 写事件上下文
@@ -54,7 +66,9 @@ public:
     IOManager(size_t threads = 1, bool use_caller = true, const std::string& name = "");
     ~IOManager();
 
+    /// 向被监听的的fb上增加某个事件
     int addEvent(int fd, Event event, std::function<void()> cb = nullptr);  // 只能用回调函数吗?
+    /// 删除被监听的某个fd上的某种事件
     bool delEvent(int fd, Event event);
     bool cancelEvent(int fd, Event event);
 
@@ -73,7 +87,7 @@ private:
     int m_epfd = 0;     // epoll的文件句柄
     /// pipe 文件句柄
     int m_tickleFds[2]; // 有消息时，向管道中写入数据，
-    /// 当前等待执行的事件数量
+    /// 当前等待执行的(读写)事件数量
     std::atomic<size_t> m_pendingEventCount = {0};
     /// IOManager的Mutex
     RWMutexType m_mutex;
